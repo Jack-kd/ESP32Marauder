c9a19e4939999ed0d2c6386a9ddc9cf7eeaeb590
Jack-kd <2030343617@qq.com>
2026-07-28 05:00:23 +0800

    Add boot logo header file

---
+++ esp32_marauder/BootLogo.h
@@ -0,0 +1,13 @@
+#pragma once
+// Boot logo for ESP32 Marauder
+#include "configs.h"
+
+#ifdef HAS_SCREEN
+
+// Generated from user image, 135x240 for small screens
+#define BLOGO_SMALL_WIDTH 135
+#define BLOGO_SMALL_HEIGHT 240
+#define BLOGO_FULL_WIDTH 240
+#define BLOGO_FULL_HEIGHT 320
+
+#endif
